f24bb074bf6b0eb6616a6e0e34d2d324f79594d0
Rob Squires <rob.squires@gmail.com>
2014-02-21 01:57:00 +0000

    fixed bug - wasn't specifying correct client conf file

--- dockman-behat.sh
+++ dockman-behat.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+red="$(tput setaf 1)"
+yel="$(tput setaf 3)"
+blue="$(tput setaf 4)"
+white="$(tput setaf 7)"
+
+reset="$(tput sgr0)"
+echo -e "${yel}  Behat ${white} + ${red} Gearman ${white} + ${blue} Docker ${reset}"
+
+
 #
 # import vars
 #
